6c24d3389099db72ac51449d34199c9346492cee
Jordan Schneidler <jordan.schneidler@gmail.com>
2016-11-16 22:05:24 -0600

    Implemented async reflect, response now only returns callback results that were successful.

--- server/database/controllers/systemController.js
+++ server/database/controllers/systemController.js
@@ -1,36 +1,45 @@
+var _ = require('lodash');
 var async = require('async');
 var shell = require('shelljs');
 
-function getIP(callback) {
+// async.reflect "Wraps the function in another function that always returns data even when it errors." - http://caolan.github.io/async/docs.html#reflect
+var getIP = async.reflect(function(callback) {
 	shell.exec('curl ipecho.net/plain', { silent: true }, function(code, stdout, stderr) {
-		callback(null, stdout);
+		callback(null, { ip: stdout });
 	});
-}
+});
 
-function getUptime(callback) {
+var getUptime = async.reflect(function(callback) {
 	shell.exec('uptime', { silent: true }, function(code, stdout, stderr) {
 		results = stdout.split(',');
 
-		callback(null, results[0].substr(1));
+		callback(null, { uptime: results[0].substr(1) });
 	});
-}
+});
 
 module.exports = {
 	index: function(req, res) {
 
+		// Do each in parallel, run callback when all are complete
 		async.parallel([
 			getIP,
 			getUptime
 		], function(err, results) {
-			if (err) return res.fail(err);
+			response = {};
 
-			res.pass(results);
+			// Append each result to response if it didn't return an error
+			_.each(results, function(result) {
+				if (result.error) return;
+				_.assign(response, result.value);
+			});
+
+			res.pass(response);
 		});
 
 	},
 
 	files: function(req, res) {
-		shell.cd('/');
+		shell.cd('~');
 		var files = shell.ls('/');
 
 		res.pass(files);
